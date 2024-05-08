1ebca51b35af5a6db33faefba2f1ba55c77e4dbb
Dionel Sylvester <dionelsylvester@MIA4MC03-DSYLVESTER-01.local>
2024-05-07 22:23:35 -0400

    Add Buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
+import feature from './assets/feature.jpg'
 import './App.css';
+import Button from './components/button';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="" alt="logo" />
+        <img src={feature} className="" alt="logo" />
+        <Button className="primary">View Menu</Button>
+        <Button className="secondary">Book Seating</Button>
+        <Button className="primary">About Us</Button>
       </header>
     </div>
   );
